2a049324c9cb87e69c8459ed57c6834fcd67fa0f
Renee Nordholm <reneenordholm@gmail.com>
2019-05-22 19:22:31 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,8 @@
 class BankAccount
   attr_accessor :name, :balance
 
-  def initialize(name, balance = 1000)
+  def initialize(name)
     @name = name
-    @balance = balance
+    @balance = 1000
   end
 end
