e975a74c1d59c6ea28021ed5a08b0295092a33f3
Renee Nordholm <reneenordholm@gmail.com>
2019-05-22 19:58:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,6 +13,11 @@ class Transfer
   end
   
   def execute_transaction
+    #bank checks the accounts are valid
+    if valid?
     #sender sends funds to receiver
+    
+    #funds sent to receiver is deducted from sender's account
+    #funds sent to receiver is added to receiver's account
   end
 end
