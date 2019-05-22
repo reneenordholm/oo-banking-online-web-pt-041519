13bc426f20af781950642cea1fee67cbfa58d755
Renee Nordholm <reneenordholm@gmail.com>
2019-05-22 20:01:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,10 @@ class Transfer
   end
   
   def reverse_transfer
-    @sender.balance += @amount
-    @receiver.balance -= @amount
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
   end
 end
